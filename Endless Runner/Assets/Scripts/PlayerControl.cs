b4f8fff36aaf883948ea9df1b92d4d1262302069
onestrangetrumpet <tgirl88888@gmail.com>
2026-02-25 14:07:51 -0500

    02/25/2026

--- Endless Runner/Assets/Scripts/PlayerControl.cs
+++ Endless Runner/Assets/Scripts/PlayerControl.cs
@@ -4,30 +4,59 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+
+    Vector3 targetPosition;
     Vector3 startPosition;
+    Vector3 velocity;
+    public float duration;
+    public float jumpStrength;
+    bool canJump;
     // Start is called before the first frame update
     void Start()
     {
-        startPosition = transform.position;
+        startPosition = new Vector3(0, 1.25f, -1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("right") && transform.position.x < 3) 
+        if (Input.GetKeyDown("right") && transform.position.x < 1) 
         {
-            transform.position += transform.right * 3;
+            targetPosition += transform.right * 3;
         }
 
-        if (Input.GetKeyDown("left") && transform.position.x > -3) 
+        if (Input.GetKeyDown("left") && transform.position.x > -1) 
         {
-            transform.position -= transform.right* 3;
+            targetPosition -= transform.right* 3;
+        }
+
+        if (Input.GetKeyDown("space") && canJump == true) 
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
+            canJump = false;
+        }
+
+        if (Input.GetKeyDown("d") && transform.position.x < 1) 
+        {
+            targetPosition += transform.right * 3;
+        }
+
+        if (Input.GetKeyDown("a") && transform.position.x > -1) 
+        {
+            targetPosition -= transform.right* 3;
         }
         
         if (Input.GetKeyDown("r")) 
         {
             transform.position = startPosition;
         }
-    }
 
+        targetPosition = new Vector3(targetPosition.x, transform.position.y, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, duration);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        canJump = true;
+    }
 }
